fd5580151f26e7a27742868a04faf37f0a4d6fad
Mohamad <mohamadborna1996@gmail.com>
2021-10-19 11:12:33 +0330

    Notification Works Well sometimes with navigate to the fragment with specific movieId

--- app/src/main/java/com/movie/android/presentation/FireBaseService.kt
+++ app/src/main/java/com/movie/android/presentation/FireBaseService.kt
@@ -3,14 +3,14 @@ package com.movie.android.presentation
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_HIGH
-import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_ONE_SHOT
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.movie.android.R
@@ -19,9 +19,6 @@ import kotlin.random.Random
 private const val CHANNEL_ID = "channel"
 
 class FireBaseService : FirebaseMessagingService() {
-    init {
-        Log.d("taggg", "Reached")
-    }
 
     override fun onNewToken(p0: String) {
         super.onNewToken(p0)
@@ -30,27 +27,46 @@ class FireBaseService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-        Log.d("taggg", "Reached")
+
+        Log.d("Firebase", message.notification!!.body!!)
+        val bundle = Bundle()
+        bundle.putInt("movieId", message.notification!!.body!!.toInt())
+
+        Log.d("Firebase", message.notification!!.body!!)
+
+        createNotification(bundle, message)
+
+    }
+
+    private fun createNotification(bundle: Bundle, message: RemoteMessage) {
         val intent = Intent(this, MainActivity::class.java)
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val notificationID = Random.nextInt()
+        val notificationID = Random.nextInt(1, 10000)
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = NavDeepLinkBuilder(this)
+            .setComponentName(MainActivity::class.java)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.detailsFragment)
+            .setArguments(bundle)
+            .createPendingIntent()
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(message.notification!!.title)
+            .setContentText("Hey!Come check out the new Movie")
+            .setSmallIcon(R.drawable.ic_baseline_crop_original_24)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createChannel(notificationManager)
         }
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_ONE_SHOT)
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(message.data["title"])
-            .setContentText(message.data["message"])
-            .setSmallIcon(R.drawable.ic_baseline_crop_original_24)
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-            .build()
-
         notificationManager.notify(notificationID, notification)
-
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
